d991110520cb26cc572535d3c5565d7d0ea82b65
Cody Gette <36174590+gettecr@users.noreply.github.com>
2018-03-19 09:31:09 -0500

    Update visualize.py

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-trainfile = '../../data/raw/train.csv'
-trainfile_processed = '../../data/processed/train_1.0.csv'
+trainfile = 'data/raw/train.csv'
+trainfile_processed = 'data/processed/train_1.0.csv'
 
 df_train = pd.read_csv(trainfile)
 df_processed= pd.read_csv(trainfile_processed)
@@ -15,11 +15,11 @@ df_processed= pd.read_csv(trainfile_processed)
 
 g = sns.FacetGrid(df_train, col='Survived', aspect=1.6)
 g.map(plt.hist, 'Age',bins=20, alpha=0.5)
-g.savefig('../../reports/figures/Age-survived.png') 
+g.savefig('reports/figures/Age-survived.png') 
 plt.close()
 
 g2=sns.barplot(data=df_train, x='Sex', y='Survived', alpha=0.5, color='tab:blue',ci=None)
-plt.savefig('../../reports/figures/Sex-survived.png')
+plt.savefig('reports/figures/Sex-survived.png')
 plt.close()
 
 g3=sns.barplot(data=df_train, x='Pclass', y='Survived', alpha=0.5, color='tab:blue',ci=None)
@@ -30,7 +30,7 @@ df_processed['Age_mod_rand'].plot.hist(alpha = 0.3, color='tab:orange',bins=20)
 df_processed['Age_mod_mean'].plot.hist(alpha = 0.3, color='tab:blue',bins=20)
 df_processed['Age'].plot.hist(alpha = 0.3, color='tab:green',bins=20)
 plt.legend(['Age_mod_rand','Age_mod_mean','Age'])
-plt.savefig('../../reports/figures/Age_distribution.png')
+plt.savefig('reports/figures/Age_distribution.png')
 plt.close()
 
 
@@ -44,13 +44,13 @@ pivot_data.drop('Survived',axis=1,inplace=True)
 pivot_data = pivot_data.reindex(["Infant","Child","Teenager","Young Adult","Adult","Senior"])
 pivot_data.plot.bar(alpha=0.5)
 plt.ylabel('% Survived')
-plt.savefig('../../reports/figures/Agebin-survived.png')
+plt.savefig('reports/figures/Agebin-survived.png')
 plt.close()
 
 g5=sns.barplot(data=df_train, x='Embarked', y='Survived', alpha=0.5, color='tab:blue',ci=None)
-plt.savefig('../../reports/figures/Embarked-survived.png')
+plt.savefig('reports/figures/Embarked-survived.png')
 plt.close()
 
 g6=sns.barplot(data=df_train, x='Embarked', y='Pclass', alpha=0.5, color='tab:blue',ci=None)
-plt.savefig('../../reports/figures/Embarked-Class.png')
+plt.savefig('reports/figures/Embarked-Class.png')
 plt.close()
